fb75d204da5d51cf62a04424256a992e3776289e
roclena <cleronga@yahoo.fr>
2020-10-24 09:36:25 +0200

    ch(user):once ligin display message and token only

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,24 +1,24 @@
-
 import User from '../services/userservices';
 import helper from '../helper/password';
 import helperlogin from '../helper/login';
 export default class UserController {
     static async signup(req, res) {
-      const inputFormData = req.body;
-      const textPassword = inputFormData.password;
-      inputFormData.password = helper.enclippass(textPassword);
-      const user = await User.createUser(inputFormData);
-     return res.json({
-         status:201,
-         message:"Acount created",
-         data:user
-     })
+        const inputFormData = req.body;
+        const textPassword = inputFormData.password;
+        inputFormData.password = helper.enclippass(textPassword);
+        const user = await User.createUser(inputFormData);
+        return res.json({
+            status: 201,
+            message: "Acount created"            
+        })
     }
-    static async login(req,res){
-        const binput=req.body.email;
-        const password=req.body.password;        
-        const user=await helperlogin.login(binput,password);
-        return res.json({user});
-
+    static async login(req, res) {
+        const binput = req.body.email;
+        const password = req.body.password;
+        const user = await helperlogin.login(binput, password);
+        return res.json({
+            message:"Log in successfully",
+            Token:user
+        });
     }
 }
